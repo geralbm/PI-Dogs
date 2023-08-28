5c93067049f0bd4d2d864d11e4cd153815ff5aa4
Geraldina Barrios <75496903+geralbm@users.noreply.github.com>
2023-08-28 20:15:13 -0300

    20230828

--- dogs/src/components/Home/Home.jsx
+++ dogs/src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
+import style from '../Home/Home.module.css';
+
 const Home = ()=>{
     return (
-        <div>
-            <h1>SOY EL HOME</h1>
+        //Pagina de Home: Que permite Buscar dogs por API y los creados en la BD.
+        <div className={style.contenedor}>
+            <h1 className={style.Titulo}>CONSULTA A "THE DOGS"</h1>
         </div>
     )
 };
